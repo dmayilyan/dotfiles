a3ff2b63ac3220b63bf6f80e24b13b5ad0c454f3
Davit Mayilyan <davit.mayilyan@protonmail.ch>
2023-09-17 00:51:19 +0400

    Add comments in the install script

--- yaylist.sh
+++ yaylist.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
+# terminal tools
 yay zsh
 yay ranger
-yay ohmyzsh
-yay black
-yay bat
-yay neovim
-yay chezmoi
 yay unzip
+yay ohmyzsh
+yay bat
+# vim and seup of it
+yay neovim
+# sound
 yay pavucontrol
+# internet
 yay firefox
 yay telegram
 yay signal
+# generic non-virtualenv python packages
+yay python-black
