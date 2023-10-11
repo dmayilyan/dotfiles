f21d3ed176516daa360a0620e8ca8f9e2686a2cd
Davit Mayilyan <davit.mayilyan@protonmail.ch>
2023-10-12 01:58:04 +0400

    fix chezmoi run. Add more install packages.

---
+++ run_yaylist.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# terminal tools
+yay zsh
+yay ranger
+yay unzip
+yay ohmyzsh
+yay bat
+# vim and setup of it
+yay neovim
+# sound
+yay pavucontrol
+# internet
+yay firefox
+yay telegram
+yay signal
+# generic non-virtualenv python packages
+yay python-pip
+yay jupyter-notebook
+yay python-black
+yay python-numpy
+yay python-pandas
+yay python-matplotlib
+yay python-scikit-learn
+yay python-seaborn
+yay python-ipywidgets
+yay python-plotly
+# other tools
+yay flameshot
+yay zathura
+yay texlive-bin
+yay texlive-basic
+yay texlive-latex
+yay texlive-binextra
+yay texlive-latexextra
